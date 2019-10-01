1eaae72bcba83b00c2b50577b7df4542b7edb670
YvonneGi <giryvonne@gmail.com>
2019-10-01 19:47:16 +0200

    file that contains functions for app features

--- app/main/views.py
+++ app/main/views.py
@@ -1,28 +1,24 @@
 from flask import render_template,request,redirect,url_for,flash,abort
 from . import main
 from .. import db
-from .forms import PostForm, CommentForm, SubscriptionForm
-from ..models import User,Writer,Post,Comment,Subscription
+from .. requests import get_quote
+from .forms import PostForm, CommentForm, SubscriptionForm,UpdatePost
+from ..models import User,Post,Comment,Subscription
 from flask_login import login_required,current_user
-from app import login_manager
-
-@login_manager.user_loader
-def load_user(user_id):
-    '''
-    @login_manager.user_loader Passes in a writer_id to this function
-    Function queries the database and gets a user's id as a response
-    '''
-    return User.query.get(user_id)
+from ..email import mail_message
 
 
-@main.route('/')
+
+
+@main.route('/',methods = ['GET','POST'])
 def index():
     
-
+    title = 'Home'
     posts=Post.query.all()
+    quote=get_quote()
 
     
-    return render_template('index.html', posts=posts)
+    return render_template('index.html', posts=posts,title=title,quote=quote)
 
 # @main.route("/")
 # @main.route("/home")
@@ -33,63 +29,115 @@ def index():
     
 #     return render_template('home.html', posts=posts)
 
-@main.route("/about")
-def about():
-    return render_template('about.html', title='About')
+# @main.route("/about")
+# def about():
+#     return render_template('about.html', title='About')
 
 
+@main.route('/post/<int:id>', methods=['GET', 'POST'])
+def single_line(id):
+    '''
+    View function to return a single article
+    '''
+    post = Post.query.get(id)
+    title = "article"
+    comments = Comment.get_comments(id)
 
-@main.route('/post', methods=['GET','POST'])
+    return render_template('post.html', post=post, title=title, comments=comments)
+
+@main.route('/new/post', methods=['GET', 'POST'])
 def new_post():
     '''
-    New Post Page
+    route to avail form for writing a new post
     '''
-    post_form = PostForm()
-
-    if post_form.validate_on_submit():
-        title = post_form.title.data
-        body = post_form.body.data
-        new_post = Post(title=title,body = body)
+    form = PostForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        post = form.body.data
+        new_post = Post(title=title, body=post)
         new_post.save_post()
         return redirect(url_for('main.index'))
+    return render_template('new_post.html', form=form)
 
-    title = f'{Post.title}'
-    return render_template('post.html',title= title, post_form=post_form)
+@main.route('/delete/post/<int:id>', methods=['GET', 'POST'])
+def delete_post(id):
+    """
+    view route to delete a selected post
+    """
+    post = Post.query.filter_by(id=id).first()
 
+    if post is None:
+        abort(404)
 
-@main.route('/comments/<int:id>', methods=['GET', 'POST'])
-@login_required
-def comments(id):
-    comment_form = CommentForm()
-    comment = Comment.query.order_by('-id').all()
-
-    if comment_form.validate_on_submit():
-        comment = comment_form.comment.data()
-        new_comment = Comment(comment=comment,users=current_user.username)
-        new_comment.save_comment()
-        return redirect(url_for('main.comments',id = post.id))
-    return render_template('comments.html')
-
-@main.route('/delete/<int:id>')
-@login_required
-def delete(id):
-    del_comment = Comment.query.get(id)
-    db.session.delete(del_comment)
-    db.session.commit()
+        post.delete_post(id)
     return redirect(url_for('main.index'))
 
-@main.route('/subscription',methods = ["GET","POST"])
+@main.route("/update/post/<int:id>", methods=['GET', 'POST'])
+@login_required
+def update_post(id):
+    post=Post.query.filter_by(id=id).first()
+    if post is None:
+        abort(404)
+
+    form = UpdatePost()
+    if form.validate_on_submit():
+        post.body = form.body.data
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('main.index'))
+    
+    return render_template('new_post.html',form=form)    
+
+
+@main.route('/post/comment/new/<int:id>', methods=['GET', 'POST'])
+@login_required
+def new_comment(id):
+    '''
+    View new comment function that returns a page with a form to create a comment for the specified post
+    '''
+    post = Post.query.filter_by(id=id).first()
+
+    if post is None:
+        abort(404)
+
+    form = CommentForm()
+
+    if form.validate_on_submit():
+        body = form.body.data
+        new_comment = Comment(
+            body=body, post_id=id, user_id=current_user.id)
+        new_comment.save_comment()
+
+        return redirect(url_for('main.index'))
+
+    title = 'New Comment'
+    return render_template('new_comment.html', title=title, comment_form=form)
+
+@main.route('/delete/comment/<int:id>',methods = ['GET','POST'])
+@login_required
+def delete(id):
+    comment = Comment.query.filter_by(id=id).first()
+    if comment is not None:
+        comment.delete_comment()
+        
+    return redirect(url_for('main.index'))
+
+@main.route('/subscription/fill',methods = ["GET","POST"])
 def subscriber():
 
     form= SubscriptionForm()
 
 
     if form.validate_on_submit():
+        name = form.name.data
         email = form.email.data
-        date = form.date.data
-        new_subscriber = Subscription(email=email,date = date,user_id=current_user.id)
-        new_subscriber.save_subscriber()
-        return redirect(url_for('subscriber'))
+        new_subscriber = Subscription(email=email,name=name)
+        db.session.add(new_subscriber)
+        db.session.commit()
+        return redirect(url_for('main.subscriber'))
+
+        mail_message("Thank you for subscribing","email/welcome_user",new_subscriber.email,user=new_subscriber)
 
 
-    return render_template('index.html',email= email, subscribe_form=form )
+    return render_template('subscription.html', form=form )
