6ebee86896fefe6d7f260681321c4b8990f4a3ce
YvonneGi <giryvonne@gmail.com>
2019-10-01 19:48:21 +0200

    file that hold models functionality

--- app/models.py
+++ app/models.py
@@ -8,7 +8,7 @@ from . import login_manager
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.get(user_id)
+    return User.query.get(int(user_id))
 
 class User(UserMixin,db.Model):
 
@@ -20,7 +20,6 @@ class User(UserMixin,db.Model):
     password = db.Column(db.Integer)
     pass_secure = db.Column(db.String(255))
     comments = db.relationship('Comment', backref = 'user', lazy = "dynamic")
-    subscriptions = db.relationship('Subscription', backref = 'user', lazy = "dynamic")
     posts = db.relationship('Post', backref = 'user', lazy = "dynamic")
 
     @property
@@ -37,30 +36,6 @@ class User(UserMixin,db.Model):
     def __repr__(self):
         return f'User {self.username}'
 
-
-class Writer(UserMixin,db.Model):
-
-    __tablename__ = 'writers'
-
-    id = db.Column(db.Integer,primary_key = True)
-    name = db.Column(db.String(255))
-    email = db.Column(db.String)
-    password = db.Column(db.Integer)
-    pass_secure = db.Column(db.String(255))
-    posts = db.relationship('Post', backref = 'writer', lazy = "dynamic")
-    comments = db.relationship('Comment', backref = 'writer', lazy = "dynamic")
-
-    @property
-    def password(self):
-        raise AttributeError('You cannot read the password attribute')
-
-    @password.setter
-    def password(self, password):
-        self.pass_secure = generate_password_hash(password)
-
-    def verify_password(self, password):
-        return check_password_hash(self.pass_secure, password)
-
 class Post(db.Model):
 
     __tablename__ = 'posts'
@@ -68,7 +43,6 @@ class Post(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     title = db.Column(db.String)
     body = db.Column(db.String)
-    writer_id = db.Column(db.Integer, db.ForeignKey('writers.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     comments = db.relationship('Comment', backref = 'post', lazy = "dynamic")
 
@@ -90,10 +64,15 @@ class Post(db.Model):
     def clear_posts(cls):
         Post.all_posts.clear()
 
-    #display posts
+    
     def get_posts(id):
         posts = Post.query.filter_by(title_id=id).all()
         return posts
+
+    def delete_post(self):
+        '''delete a given post from the database'''
+        db.session.delete(self)
+        db.session.commit()
 
 
 
@@ -105,7 +84,25 @@ class Comment(db.Model):
     body = db.Column(db.String)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
-    writer_id = db.Column(db.Integer, db.ForeignKey('writers.id'))
+  
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def clear_comments(cls):
+        Comment.all_comments.clear()
+
+    @classmethod
+    def get_comments(cls,id):
+        comments=Comment.query.filter_by(post_id=id).all()
+        return comments
+    
+
+    def delete_comment(self):
+        db.session.delete(self)
+        db.session.commit()
 
 class Subscription(db.Model):
 
@@ -114,5 +111,14 @@ class Subscription(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     email = db.Column(db.String)
     name = db.Column(db.String)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    
+
+class Quote:
+
+    def __init__(self,id,author,content):
+        self.id=id
+        self.author=author
+        self.content=content
+        
+
     
